bff76c023f8d4d87ba54dbe0642c30d74212927b
cvrain <cvraindays@outlook.com>
2025-01-27 13:48:08 +0800

    add login and register page

---
+++ src/components/register-form.tsx
@@ -0,0 +1,44 @@
+import {cn} from "@/lib/utils"
+import {Button} from "@/components/ui/button"
+import {Input} from "@/components/ui/input"
+import {Label} from "@/components/ui/label"
+
+export function RegisterForm({
+                                 className,
+                                 ...props
+                             }: React.ComponentPropsWithoutRef<"form">) {
+    return (
+        <form className={cn("flex flex-col gap-6", className)} {...props}>
+            <div className="flex flex-col items-center gap-2 text-center">
+                <h1 className="text-2xl font-bold">Register for an account</h1>
+                <p className="text-balance text-sm text-muted-foreground">
+                    Enter your details to create a new account
+                </p>
+            </div>
+            <div className="grid gap-6">
+                <div className="grid gap-2">
+                    <Label htmlFor="name">Name</Label>
+                    <Input id="name" type="text" placeholder="John Doe" required/>
+                </div>
+                <div className="grid gap-2">
+                    <Label htmlFor="email">Email</Label>
+                    <Input id="email" type="email" placeholder="m@example.com" required/>
+                </div>
+                <div className="grid gap-2">
+                    <Label htmlFor="password">Password</Label>
+                    <Input id="password" type="password" required/>
+                </div>
+                <Button type="submit" className="w-full">
+                    Register
+                </Button>
+
+            </div>
+            <div className="text-center text-sm">
+                Already have an account?{" "}
+                <a href="/login" className="underline underline-offset-4">
+                    Login
+                </a>
+            </div>
+        </form>
+    )
+}
