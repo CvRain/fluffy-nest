47cab626193674aee3cc4651bdcfceba1d6fddee
cvrain <cvraindays@outlook.com>
2025-03-15 21:33:50 +0800

    学习了一下动态路由

---
+++ src/routes/blog/[...slug]/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import {page} from '$app/stores'
+
+    console.log($page.params.slug)
+    const slugArr = $page.params.slug.split("/")
+
+    slugArr.forEach((value, index, array) =>{
+        console.log(index, ":", value)
+    })
+</script>
+
+{#if slugArr.length === 1}
+    <h1>Viewing docs for feature{slugArr[0]}</h1>
+{:else if slugArr.length === 2}
+    <h1>Viewing docs for feature{slugArr[2]}</h1>
+{/if}
