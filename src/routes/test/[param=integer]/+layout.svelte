00718e7c12b504d6f37a3c3aa3f5d2368c5d486e
cvrain <cvraindays@outlook.com>
2025-03-17 21:12:28 +0800

    增加实验性网页

---
+++ src/routes/test/[param=integer]/+layout.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+    import Slash from "@lucide/svelte/icons/slash";
+    import * as Breadcrumb from "$lib/components/ui/breadcrumb/index.js";
+</script>
+
+<Breadcrumb.Root>
+    <Breadcrumb.List>
+        <Breadcrumb.Item>
+            <Breadcrumb.Link href="/">Home</Breadcrumb.Link>
+        </Breadcrumb.Item>
+        <Breadcrumb.Separator>
+            <Slash/>
+        </Breadcrumb.Separator>
+        <Breadcrumb.Item>
+            <Breadcrumb.Link href="/test">Test</Breadcrumb.Link>
+        </Breadcrumb.Item>
+        <Breadcrumb.Separator>
+            <Slash/>
+        </Breadcrumb.Separator>
+        <Breadcrumb.Item>
+            <Breadcrumb.Page>numbers</Breadcrumb.Page>
+        </Breadcrumb.Item>
+    </Breadcrumb.List>
+</Breadcrumb.Root>
+
+<slot/>
