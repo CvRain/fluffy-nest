53fc5c030844c41b368f930ecffef428d18a86c3
cvrain <cvraindays@outlook.com>
2025-03-18 23:18:30 +0800

    准备构建主页

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,42 @@
 <script lang="ts">
-	import { goto } from '$app/navigation';
-	import { onMount } from 'svelte';
+    import * as Card from '$lib/components/ui/card';
+    import * as Resizable from '$lib/components/ui/resizable';
+    import Textarea from '@/components/ui/textarea/textarea.svelte';
+    import MarkdownPreview from '@/components/markdown-preview.svelte';
 
-	onMount(() => {
-		goto('/auth/login');
-	})
+    let markdownText: string
+
 </script>
 
-<main>
-</main>
+
+<Card.Root>
+    <Card.Content>
+        <Resizable.PaneGroup direction="horizontal" class="min-h-[200px] rounded-lg border">
+            <Resizable.Pane>
+                <Textarea
+                    placeholder="write your idea~"
+                    bind:value={markdownText}
+                    class="full-height .hide-scrollbar"
+                ></Textarea>
+            </Resizable.Pane>
+            <Resizable.Handle withHandle/>
+            <Resizable.Pane defaultSize={45}>
+                <MarkdownPreview {markdownText}/>
+            </Resizable.Pane>
+        </Resizable.PaneGroup>
+    </Card.Content>
+</Card.Root>
+
+<style>
+    :global(.full-height) {
+		height: 100vh;
+	}
+
+	:global(.hide-scrollbar) {
+		scrollbar-width: none;
+	}
+
+	:global(.hide-scrollbar)::-webkit-scrollbar {
+		display: none; /* Chrome, Safari and Opera */
+	}
+</style>
